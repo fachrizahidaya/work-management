dfd503d9dfc8c6f00a61daf9292a6d95ea6867f8
jrmygp <fat.snorlax17@gmail.com>
2023-10-04 13:04:33 +0700

    Feat : Calendar with deadlines and holidays

--- src/components/shared/CalendarWithSlider.js
+++ src/components/shared/CalendarWithSlider.js
@@ -1,34 +1,15 @@
-import { useState } from "react";
-
 import { Agenda } from "react-native-calendars";
+
 import dayjs from "dayjs";
 
-import { Image, Text, VStack, View } from "native-base";
-import { TouchableOpacity, StyleSheet, Alert } from "react-native";
+import { Image, Text, VStack } from "native-base";
+import { TouchableOpacity, StyleSheet } from "react-native";
 
-const CalendarWithSlider = ({ selectedDate }) => {
-  const [items, setItems] = useState({});
-  //   date format is YYYY-MM-DD
-  const dateConverter = dayjs().format(selectedDate);
-
-  const date = {
-    ["2023-10-01"]: [
-      { name: "Task", time: "09:00", description: "Deadline task", height: 200 },
-      { name: "Project", time: "10:00", description: "Deadline project", height: 200 },
-    ],
-    ["2023-10-02"]: [
-      { name: "Task", time: "09:00", description: "Deadline task", height: 200 },
-      { name: "Project", time: "10:00", description: "Deadline project", height: 200 },
-    ],
-  };
-
+const CalendarWithSlider = ({ items }) => {
+  const today = dayjs().format("YYYY-MM-DD");
   const renderItem = (reservation) => {
     return (
-      <TouchableOpacity
-        style={[styles.item, { height: reservation.height }]}
-        onPress={() => Alert.alert(reservation.name)}
-      >
-        <Text>{reservation.name}</Text>
+      <TouchableOpacity style={styles.item}>
         <Text>{reservation.description}</Text>
       </TouchableOpacity>
     );
@@ -36,13 +17,14 @@ const CalendarWithSlider = ({ selectedDate }) => {
 
   return (
     <Agenda
-      items={date}
+      items={items}
       showClosingKnob={true}
+      selected={today}
       renderItem={renderItem}
       theme={{
-        agendaTodayColor: "red",
         selectedDayTextColor: "white",
         selectedDayBackgroundColor: "#176688",
+        todayTextColor: "#176688",
       }}
       renderEmptyData={() => {
         return (
@@ -72,18 +54,6 @@ const styles = StyleSheet.create({
     padding: 10,
     marginRight: 10,
     marginTop: 17,
-  },
-  emptyDate: {
-    height: 15,
-    flex: 1,
-    paddingTop: 30,
-  },
-  customDay: {
-    margin: 10,
-    fontSize: 24,
-    color: "green",
-  },
-  dayItem: {
-    marginLeft: 34,
+    justifyContent: "center",
   },
 });
--- src/screens/Band/Calendar.js
+++ src/screens/Band/Calendar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { SafeAreaView } from "react-native";
 
@@ -7,14 +7,54 @@ import { useFetch } from "../../hooks/useFetch";
 
 const CalendarScreen = () => {
   const { data: projectDeadlines } = useFetch("/pm/projects/deadline");
-  // const [formattedProjectDeadlines, setFormattedProjectDeadlines] = useState({});
+  const { data: holidays } = useFetch("/hr/holidays/calendar");
+  const { data: taskDeadlines } = useFetch("/pm/tasks/deadline");
 
-  const formatted = Object.assign({}, projectDeadlines?.data);
-  console.log(formatted);
+  const formattedProjectDeadlines = {};
+  const formattedTaskDeadlines = {};
+  const formattedHolidays = {};
+
+  projectDeadlines?.data.forEach((item) => {
+    const date = item.date.split("-").reverse().join("-"); // Convert date format
+    const key = `${date.slice(0, 7)}-01`; // Truncate to the first day of the month
+    const value = { description: item.description };
+
+    if (!formattedProjectDeadlines[key]) {
+      formattedProjectDeadlines[key] = [value];
+    } else {
+      formattedProjectDeadlines[key].push(value);
+    }
+  });
+
+  taskDeadlines?.data.forEach((item) => {
+    const date = item.date.split("-").reverse().join("-"); // Convert date format
+    const key = `${date.slice(0, 7)}-01`; // Truncate to the first day of the month
+    const value = { description: item.description };
+
+    if (!formattedTaskDeadlines[key]) {
+      formattedTaskDeadlines[key] = [value];
+    } else {
+      formattedTaskDeadlines[key].push(value);
+    }
+  });
+
+  holidays?.data.forEach((item) => {
+    const date = item.date.split("-").reverse().join("-"); // Convert date format
+    const key = `${date.slice(0, 7)}-01`; // Truncate to the first day of the month
+    const value = { description: item.description };
+
+    if (!formattedHolidays[key]) {
+      formattedHolidays[key] = [value];
+    } else {
+      formattedHolidays[key].push(value);
+    }
+  });
+
+  const finalResult = { ...formattedHolidays, ...formattedProjectDeadlines, ...formattedTaskDeadlines };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <CalendarWithSlider />
+      <CalendarWithSlider items={finalResult} />
     </SafeAreaView>
   );
 };
