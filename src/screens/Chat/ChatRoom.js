0bfcaba2011a88e14e3c37bdda1d3b00269e8b67
jrmygp <fat.snorlax17@gmail.com>
2023-10-05 15:00:26 +0700

    Feat : Chat rooms now connected with socket

--- src/screens/Chat/ChatRoom.js
+++ src/screens/Chat/ChatRoom.js
@@ -2,16 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 
 import Echo from "laravel-echo";
-import {
-  Pusher,
-  PusherMember,
-  PusherChannel,
-  PusherEvent,
-  PusherAuthorizerResult,
-} from "@pusher/pusher-websocket-react-native";
+import Pusher from "pusher-js/react-native";
+
 import { useSelector } from "react-redux";
 
-import { FlatList, Flex } from "native-base";
+import { FlashList } from "@shopify/flash-list";
+import { Flex } from "native-base";
 import { SafeAreaView } from "react-native";
 
 import ChatBubble from "../../components/Chat/ChatBubble/ChatBubble";
@@ -21,69 +17,58 @@ import ChatInput from "../../components/Chat/ChatInput/ChatInput";
 import { useKeyboardChecker } from "../../hooks/useKeyboardChecker";
 
 const ChatRoom = () => {
+  window.Pusher = Pusher;
   const route = useRoute();
   const { name, userId, image } = route.params;
-  const pusher = Pusher.getInstance();
   const navigation = useNavigation();
   const userSelector = useSelector((state) => state.auth);
-  const [chatList, setChatList] = useState([]);
   const { isKeyboardVisible, keyboardHeight } = useKeyboardChecker();
+  const [chatList, setChatList] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
 
-  // const echo = new Echo({
-  //   broadcaster: "pusher",
-  //   key: "kssapp",
-  //   wsHost: "api-dev.kolabora-app.com",
-  //   wsPort: 6001,
-  //   wssport: 6001,
-  //   transports: ["websocket"],
-  //   enabledTransports: ["ws", "wss"],
-  //   forceTLS: false,
-  //   disableStats: true,
-  //   cluster: "mt1",
-  // });
+  const echo = new Echo({
+    broadcaster: "pusher",
+    key: "kssapp",
+    wsHost: "api-dev.kolabora-app.com",
+    wsPort: 6001,
+    wssport: 6001,
+    transports: ["websocket"],
+    enabledTransports: ["ws", "wss"],
+    forceTLS: false,
+    disableStats: true,
+    cluster: "mt1",
+  });
 
-  const getPersonalChat = async () => {
-    try {
-      await pusher.init({
-        apiKey: "kssapp",
-        cluster: "mt1",
-        authEndpoint: "api-dev.kolabora-app.com",
-        onAuthorizer,
-        onConnectionStateChange,
-        onError,
-        onEvent,
-        onSubscriptionSucceeded,
-        onSubscriptionError,
-        onSubscriptionCount,
-        onDecryptionFailure,
-        onMemberAdded,
-        onMemberRemoved,
-      });
-
-      await pusher.subscribe({
-        channelName: `personal.chat.${userSelector.id}.${userId}`,
-        onEvent: (event) => {
-          if (event.eventName === `.personal.chat`) {
-            console.log(event);
-            setChatList(event.data);
-          }
-        },
-      });
-      pusher.connect();
-    } catch (error) {
-      console.log(error);
-    }
+  // PERSONAL CHAT
+  const getPersonalChat = () => {
+    echo.channel(`personal.chat.${userSelector.id}.${userId}`).listen(".personal.chat", (event) => {
+      setChatList((currentChats) => [...currentChats, event.data]);
+    });
   };
 
   const getPersonalMessage = async () => {
-    const res = await axiosInstance.get(`/chat/personal/${userSelector.id}/${userId}/message`, {
-      params: {
-        offset: 0,
-      },
-    });
-
-    if (res) {
-      setChatList(res.data.data);
+    try {
+      if (hasMore) {
+        const res = await axiosInstance.get(`/chat/personal/${userSelector.id}/${userId}/message`, {
+          params: {
+            offset: offset,
+            limit: 20,
+          },
+        });
+        setChatList((currentChats) => [...res.data.data, ...currentChats]);
+        // setChatList((currentChats) => {
+        //   if (currentChats.length !== currentChats.length + res.data.data.length) {
+        //     return [...res.data.data, ...currentChats];
+        //   } else {
+        //     setHasMore(false);
+        //     return currentChats;
+        //   }
+        // });
+        // setOffset((prevState) => prevState + 20);
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -98,9 +83,18 @@ const ChatRoom = () => {
     <SafeAreaView style={{ flex: 1, backgroundColor: "#FAFAFA", marginBottom: isKeyboardVisible ? keyboardHeight : 0 }}>
       <ChatHeader name={name} navigation={navigation} />
 
-      <Flex flex={1} bg="#FAFAFA" paddingX={2}>
-        <FlatList
+      <Flex
+        flex={1}
+        bg="#FAFAFA"
+        paddingX={2}
+        // style={{ display: "flex", flexDirection: "column-reverse" }}
+      >
+        <FlashList
           inverted
+          keyExtractor={(item, index) => index}
+          onEndReachedThreshold={0.1}
+          // onEndReached={getPersonalMessage}
+          estimatedItemSize={200}
           data={chatList}
           renderItem={({ item }) => (
             <ChatBubble
