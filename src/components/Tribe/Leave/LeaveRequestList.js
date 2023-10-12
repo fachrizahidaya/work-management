8f42a4eb6000de0b507e6d37e8b6662633fd76b3
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-10-12 16:04:01 +0700

    fix: team leave feat: change pin payslip

--- src/components/Tribe/Leave/LeaveRequestList.js
+++ src/components/Tribe/Leave/LeaveRequestList.js
@@ -41,70 +41,76 @@ const LeaveRequestList = ({
 
   return (
     <>
-      <Box gap={2} borderTopColor="#E8E9EB" borderTopWidth={1} py={3} px={3}>
-        <Flex flexDir="row" justifyContent="space-between" alignItems="center">
-          <Text fontWeight={500} fontSize={14} color="#3F434A">
-            {leaveName}
-          </Text>
-          {status !== "Pending" ? null : (
-            <Pressable onPress={toggleAction}>
-              <Icon
-                as={<MaterialCommunityIcons name="dots-vertical" />}
-                size="md"
-                borderRadius="full"
-                color="#000000"
-              />
-            </Pressable>
-          )}
-          <Actionsheet isOpen={actionIsOpen} onClose={toggleAction}>
-            <Actionsheet.Content>
-              <Actionsheet.Item onPress={toggleCancelModal}>Cancel Request</Actionsheet.Item>
-            </Actionsheet.Content>
-          </Actionsheet>
-          <ConfirmationModal
-            isOpen={cancelModalIsOpen}
-            toggle={toggleCancelModal}
-            apiUrl={`/hr/leave-requests/${id}/cancel`}
-            color="red.600"
-            hasSuccessFunc={true}
-            header="Cancel Leave Request"
-            onSuccess={() => {
-              toggleAction();
-              refetchPersonalLeaveRequest();
-              refetchProfile();
-            }}
-            description="Are you sure to cancel this request?"
-            successMessage="Request canceled"
-            isDelete={false}
-            isPatch={true}
-          />
-        </Flex>
-        <Flex flexDir="row" justifyContent="space-between" alignItems="center">
-          <Flex flex={1}>
+      {status === "Canceled" ? null : (
+        <Box gap={2} borderTopColor="#E8E9EB" borderTopWidth={1} py={3} px={3}>
+          <Flex flexDir="row" justifyContent="space-between" alignItems="center">
+            <Text fontWeight={500} fontSize={14} color="#3F434A">
+              {leaveName}
+            </Text>
+            {status !== "Pending" ? null : (
+              <Pressable onPress={toggleAction}>
+                <Icon
+                  as={<MaterialCommunityIcons name="dots-vertical" />}
+                  size="md"
+                  borderRadius="full"
+                  color="#000000"
+                />
+              </Pressable>
+            )}
+            <Actionsheet isOpen={actionIsOpen} onClose={toggleAction}>
+              <Actionsheet.Content>
+                <Actionsheet.Item onPress={toggleCancelModal}>Cancel Request</Actionsheet.Item>
+              </Actionsheet.Content>
+            </Actionsheet>
+            <ConfirmationModal
+              isOpen={cancelModalIsOpen}
+              toggle={toggleCancelModal}
+              apiUrl={`/hr/leave-requests/${id}/cancel`}
+              color="red.600"
+              hasSuccessFunc={true}
+              header="Cancel Leave Request"
+              onSuccess={() => {
+                toggleAction();
+                refetchPersonalLeaveRequest();
+                refetchProfile();
+              }}
+              description="Are you sure to cancel this request?"
+              successMessage="Request canceled"
+              isDelete={false}
+              isPatch={true}
+            />
+          </Flex>
+          <Flex flexDir="row" justifyContent="space-between" alignItems="center">
+            <Flex flex={1}>
+              <Text color="#595F69" fontSize={12} fontWeight={400}>
+                {reason}
+              </Text>
+            </Flex>
+            <Badge borderRadius={10} w={20}>
+              <Flex gap={2} flexDir="row">
+                <Icon as={<MaterialCommunityIcons name="clock-outline" />} size={5} color="#186688" />
+                {days > 1 ? `${days} days` : `${days} day`}
+              </Flex>
+            </Badge>
+          </Flex>
+          <Flex flexDir="row" justifyContent="space-between" alignItems="center">
             <Text color="#595F69" fontSize={12} fontWeight={400}>
-              {reason}
+              {dayjs(startDate).format("DD.MM.YYYY")} - {dayjs(endDate).format("DD.MM.YYYY")}
+            </Text>
+            <Text
+              color={
+                status === "Pending"
+                  ? "#F0C290"
+                  : status === "Declined" || status === "Canceled"
+                  ? "#FF6262"
+                  : "#437D96"
+              }
+            >
+              {status}
             </Text>
           </Flex>
-          <Badge borderRadius={10} w={20}>
-            <Flex gap={2} flexDir="row">
-              <Icon as={<MaterialCommunityIcons name="clock-outline" />} size={5} color="#186688" />
-              {days > 1 ? `${days} days` : `${days} day`}
-            </Flex>
-          </Badge>
-        </Flex>
-        <Flex flexDir="row" justifyContent="space-between" alignItems="center">
-          <Text color="#595F69" fontSize={12} fontWeight={400}>
-            {dayjs(startDate).format("DD.MM.YYYY")} - {dayjs(endDate).format("DD.MM.YYYY")}
-          </Text>
-          <Text
-            color={
-              status === "Pending" ? "#F0C290" : status === "Declined" || status === "Canceled" ? "#FF6262" : "#437D96"
-            }
-          >
-            {status}
-          </Text>
-        </Flex>
-      </Box>
+        </Box>
+      )}
     </>
   );
 };
--- src/screens/Tribe/PayslipScreen.js
+++ src/screens/Tribe/PayslipScreen.js
@@ -1,13 +1,23 @@
+import { useFormik } from "formik";
+
 import { SafeAreaView, StyleSheet } from "react-native";
-import { Button, Flex, Image, Skeleton, Text, VStack } from "native-base";
+import { Button, Flex, Image, Skeleton, Text, VStack, useToast } from "native-base";
 import { FlashList } from "@shopify/flash-list";
 import { RefreshControl } from "react-native-gesture-handler";
 
 import PayslipList from "../../components/Tribe/Payslip/PayslipList";
 import { useFetch } from "../../hooks/useFetch";
 import PageHeader from "../../components/shared/PageHeader";
+import axiosInstance from "../../config/api";
+import PayslipPasswordEdit from "../../components/Tribe/Payslip/PayslipPasswordEdit";
+import { useDisclosure } from "../../hooks/useDisclosure";
+import { SuccessToast } from "../../components/shared/ToastDialog";
 
 const PayslipScreen = () => {
+  const { isOpen: formIsOpen, toggle: toggleForm } = useDisclosure(false);
+
+  const toast = useToast();
+
   const {
     data: payslip,
     refetch: refetchPayslip,
@@ -15,15 +25,35 @@ const PayslipScreen = () => {
     isLoading: payslipIsLoading,
   } = useFetch("/hr/payslip");
 
+  const payslipPasswordUpdateHandler = async (data, setSubmitting, setStatus) => {
+    try {
+      const res = await axiosInstance.patch(`/hr/payslip/change-password`, data);
+      setSubmitting(false);
+      setStatus("success");
+      refetchPayslip();
+      toast.show({
+        render: () => {
+          return <SuccessToast message={"Password Updated"} />;
+        },
+        placement: "top",
+      });
+    } catch (err) {
+      console.log(err);
+      setSubmitting(false);
+      setStatus("error");
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Flex flexDir="row" alignItems="center" justifyContent="space-between" bgColor="#FFFFFF" py={14} px={15}>
         <PageHeader title="My Payslip" backButton={false} />
-        <Button>
+        <Button onPress={() => toggleForm()}>
           <Text color="#FFFFFF" fontSize={12} fontWeight={500}>
             Change PIN
           </Text>
         </Button>
+        <PayslipPasswordEdit formIsOpen={formIsOpen} toggleForm={toggleForm} onSubmit={payslipPasswordUpdateHandler} />
       </Flex>
 
       {!payslipIsLoading ? (
