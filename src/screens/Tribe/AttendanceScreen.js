a04067e0af88058827ecb4d5ee8be27588c478de
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-10-12 11:41:22 +0700

    fix: attendance report

--- src/screens/Tribe/AttendanceScreen.js
+++ src/screens/Tribe/AttendanceScreen.js
@@ -30,8 +30,6 @@ const AttendanceScreen = () => {
     refetch: refetchAttendanceData,
   } = useFetch(`/hr/timesheets/personal`, [filter], attendanceFetchParameters);
 
-  console.log(attendanceData?.data);
-
   const handleMonthChange = (newMonth) => {
     setFilter(newMonth);
   };
