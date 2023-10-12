93b89d229ae5b6d5ccfe3195dbbea85e2834fcfc
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-10-12 11:41:22 +0700

    fix: attendance report

--- src/screens/Tribe/AttendanceScreen.js
+++ src/screens/Tribe/AttendanceScreen.js
@@ -2,18 +2,25 @@ import { useState } from "react";
 import dayjs from "dayjs";
 
 import { SafeAreaView, StyleSheet } from "react-native";
-import { Flex, Text } from "native-base";
+import { Flex, Text, useToast } from "native-base";
 
 import AttendanceCalendar from "../../components/Tribe/Attendance/AttendanceCalendar";
 import { useFetch } from "../../hooks/useFetch";
 import Schedule from "../../components/Tribe/Calendar/Schedule";
 import PageHeader from "../../components/shared/PageHeader";
+import axiosInstance from "../../config/api";
+import { useDisclosure } from "../../hooks/useDisclosure";
+import { SuccessToast } from "../../components/shared/ToastDialog";
 
 const AttendanceScreen = () => {
   const [filter, setFilter] = useState({
     month: dayjs().format("M"),
     year: dayjs().format("YYYY"),
   });
+
+  const { isOpen: reportIsOpen, toggle: toggleReport } = useDisclosure(false);
+
+  const toast = useToast();
 
   const attendanceFetchParameters = filter;
 
@@ -23,10 +30,28 @@ const AttendanceScreen = () => {
     refetch: refetchAttendanceData,
   } = useFetch(`/hr/timesheets/personal`, [filter], attendanceFetchParameters);
 
-  console.log(attendanceData?.data);
-
   const handleMonthChange = (newMonth) => {
     setFilter(newMonth);
+  };
+
+  const attendanceReportSubmitHandler = async (attendance_id, data, setSubmitting, setStatus) => {
+    try {
+      const res = await axiosInstance.patch(`/hr/timesheets/personal/${attendance_id}`, data);
+      refetchAttendanceData();
+      setStatus("success");
+      setSubmitting(false);
+      toast.show({
+        render: () => {
+          return <SuccessToast message={"Report Submitted"} />;
+        },
+        placement: "top",
+      });
+      toggleReport();
+    } catch (err) {
+      console.log(err);
+      setStatus("error");
+      setSubmitting(false);
+    }
   };
 
   return (
@@ -35,7 +60,13 @@ const AttendanceScreen = () => {
         <Flex flexDir="row" alignItems="center" justifyContent="space-between" bgColor="white" py={14} px={15}>
           <PageHeader width={200} title="My Attendance History" backButton={false} />
         </Flex>
-        <AttendanceCalendar attendance={attendanceData?.data} onMonthChange={handleMonthChange} />
+        <AttendanceCalendar
+          attendance={attendanceData?.data}
+          onMonthChange={handleMonthChange}
+          onSubmit={attendanceReportSubmitHandler}
+          reportIsOpen={reportIsOpen}
+          toggleReport={toggleReport}
+        />
       </SafeAreaView>
     </>
   );
