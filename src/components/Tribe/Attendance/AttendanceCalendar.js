0a5c80ad33f50c6ab7f02c06de7fcf99922ba460
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-10-12 11:41:22 +0700

    fix: attendance report

--- src/components/Tribe/Attendance/AttendanceCalendar.js
+++ src/components/Tribe/Attendance/AttendanceCalendar.js
@@ -1,5 +1,7 @@
 import React, { useState, Fragment, useCallback, useMemo, useRef, useEffect } from "react";
 import dayjs from "dayjs";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 import { StyleSheet, Dimensions } from "react-native";
 import { Calendar } from "react-native-calendars";
@@ -8,20 +10,15 @@ import { Flex, FormControl, Icon, Input, Modal, ScrollView, Select, Text, VStack
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 import testIDs from "../testIDs";
-import { useDisclosure } from "../../../hooks/useDisclosure";
 import FormButton from "../../shared/FormButton";
 
-const AttendanceCalendar = ({ attendance, onMonthChange }) => {
+const AttendanceCalendar = ({ attendance, onMonthChange, onSubmit, reportIsOpen, toggleReport }) => {
   const [selected, setSelected] = useState(INITIAL_DATE);
   const [currentMonth, setCurrentMonth] = useState(INITIAL_DATE);
   const [items, setItems] = useState({});
   const [date, setDate] = useState({});
 
-  const { isOpen: reportIsOpen, toggle: toggleReport } = useDisclosure(false);
-
   const INITIAL_DATE = dayjs().format("YYYY-MM-DD");
-
-  const { height } = Dimensions.get("window");
 
   /**
    * Status attendance Handler
@@ -30,26 +27,35 @@ const AttendanceCalendar = ({ attendance, onMonthChange }) => {
   const reportRequired = { key: "reportRequired", color: "#FDC500", name: "Report Required" };
   const submittedReport = { key: "submittedReport", color: "#186688", name: "Submitted Report" };
   const dayOff = { key: "dayOff", color: "#3bc14a", name: "Day-off" };
-  const sick = { key: "sick", color: "black", name: "Sick" };
+  const sick = { key: "sick", color: "#000000", name: "Sick" };
 
+  /**
+   * Late type Handler
+   */
   const lateType = [
     { id: 1, name: "Late" },
     { id: 2, name: "Permit" },
     { id: 3, name: "Other" },
   ];
 
+  /**
+   * Early type Handler
+   */
   const earlyType = [
     { id: 1, name: "Early" },
     { id: 2, name: "Permit" },
     { id: 3, name: "Other" },
   ];
 
+  /**
+   * List icon for Report status
+   */
   const listIcons = [
     { key: "allGood", color: "#EDEDED", name: "All Good" },
     { key: "reportRequired", color: "#FDC500", name: "Report Required" },
     { key: "submittedReport", color: "#186688", name: "Submitted Report" },
     { key: "dayOff", color: "#3bc14a", name: "Day-off" },
-    { key: "sick", color: "black", name: "Sick" },
+    { key: "sick", color: "#000000", name: "Sick" },
   ];
 
   const handleMonthChange = (newMonth) => {
@@ -81,6 +87,11 @@ const AttendanceCalendar = ({ attendance, onMonthChange }) => {
     };
   }, [selected]);
 
+  /**
+   *
+   * Input date to Calendar Handler
+   */
+
   useEffect(() => {
     if (attendance && attendance.length > 0) {
       let dateList = {};
@@ -88,6 +99,7 @@ const AttendanceCalendar = ({ attendance, onMonthChange }) => {
       attendance.forEach((item) => {
         dateList[item?.date] = [
           {
+            id: item?.id,
             attendanceReason: item?.att_reason,
             attendanceType: item?.att_type,
             timeIn: item?.time_in,
@@ -111,13 +123,23 @@ const AttendanceCalendar = ({ attendance, onMonthChange }) => {
     }
   }, [attendance]);
 
+  /**
+   * Toggle date Handler
+   * @param {*} day
+   */
+
   const toggleDateHandler = (day) => {
     if (day) {
       const selectedDate = day.dateString;
       const dateData = items[selectedDate];
       if (dateData && dateData.length > 0) {
         dateData.map((item) => {
-          if (item?.date && item?.confirmation === 0) {
+          if (
+            item?.date &&
+            item?.confirmation === 0 &&
+            item?.attendanceType !== "Permit" &&
+            item?.attendanceType !== "Leave"
+          ) {
             toggleReport();
             setDate(item);
           }
@@ -126,7 +148,28 @@ const AttendanceCalendar = ({ attendance, onMonthChange }) => {
     }
   };
 
-  console.log(`1`, date);
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      late_type: date?.lateType || "",
+      late_reason: date?.lateReason || "",
+      early_type: date?.earlyType || "",
+      early_reason: date?.earlyReason || "",
+      att_type: date?.attendanceType || "",
+      att_reason: date?.attendanceReason || "",
+    },
+    // validationSchema: yup.object().shape({}),
+    onSubmit: (values, { resetForm, setSubmitting, setStatus }) => {
+      setStatus("processing");
+      onSubmit(date?.id, values, setSubmitting, setStatus);
+      resetForm();
+    },
+  });
+
+  /**
+   * Marked dates in Calendar Handler
+   * @returns
+   */
 
   const renderCalendarWithMultiDotMarking = () => {
     const markedDates = {};
@@ -137,7 +180,11 @@ const AttendanceCalendar = ({ attendance, onMonthChange }) => {
         events.forEach((event) => {
           let dotColor = "";
 
-          if (event.dayType === "Weekend") {
+          if (
+            event.dayType === "Weekend" ||
+            (event?.attendanceType === "Leave" && event?.dayType === "Work Day") ||
+            (event?.attendanceType === "Permit" && event?.dayType === "Work Day")
+          ) {
             dotColor = dayOff.color;
           } else if (
             (event?.early && !event?.earlyReason && !event?.confirmation) ||
@@ -151,7 +198,7 @@ const AttendanceCalendar = ({ attendance, onMonthChange }) => {
             (event?.attendanceType !== "Alpa" && event?.attendanceReason)
           ) {
             dotColor = submittedReport.color;
-          } else if (event?.attendanceType === "Sick") {
+          } else if (event?.attendanceType === "Sick" || event?.attendanceType === "Leave") {
             dotColor = sick.color;
           } else if (
             (event?.confirmation && event?.dayType !== "Weekend") ||
@@ -168,6 +215,12 @@ const AttendanceCalendar = ({ attendance, onMonthChange }) => {
       }
     }
 
+    // useEffect(() => {
+    //   if (!formik.isSubmitting && formik.status === "success") {
+    //     toggleReport(formik.resetForm);
+    //   }
+    // }, [formik.isSubmitting, formik.status]);
+
     return (
       <Fragment>
         <Calendar
@@ -183,111 +236,97 @@ const AttendanceCalendar = ({ attendance, onMonthChange }) => {
           <Modal.Content>
             <Modal.CloseButton />
             <Modal.Header>{dayjs(date?.date).format("dddd, DD MMM YYYY")}</Modal.Header>
-            {(date?.late && !date?.lateType) ||
-              (date?.late && date?.lateType && (
-                <Modal.Body>
-                  <VStack
-                    w="95%"
-                    space={3}
-                    // pb={keyboardHeight}
-                  >
-                    <VStack w="100%" space={2}>
-                      <FormControl
-                      // isInvalid={formik.errors.leave_id}
-                      >
-                        <FormControl.Label>Late Type</FormControl.Label>
-                      </FormControl>
-                      <Select
-                        // mt={-3}
-                        // selectedValue={formik.values.leave_id}
-                        // onValueChange={(value) => formik.setFieldValue("leave_id", value)}
-                        borderRadius={15}
-                        borderWidth={1}
-                        variant="unstyled"
-                        // key="leave_id"
-                        placeholder="Select Late Type"
-                        dropdownIcon={<Icon as={<MaterialCommunityIcons name="chevron-down" />} size="lg" mr={2} />}
-                      >
-                        {lateType.map((item) => {
-                          return <Select.Item label={item?.name} value={item?.id} key={item?.id} />;
-                        })}
-                      </Select>
-                      <FormControl
-                        mt={-2}
-                        // isInvalid={formik.errors.leave_id}
-                      >
-                        {/* <FormControl.ErrorMessage>{formik.errors.leave_id}</FormControl.ErrorMessage> */}
-                      </FormControl>
-                      <FormControl.Label>Reason</FormControl.Label>
 
-                      <FormControl
-                      // isInvalid={formik.errors.password}
-                      >
-                        <Input
-                          variant="outline"
-                          placeholder="Enter your reason"
-                          // value={formik.values.password}
-                          // onChangeText={(value) => formik.setFieldValue("password", value)}
-                        />
-                        {/* <FormControl.ErrorMessage>{formik.errors.password}</FormControl.ErrorMessage> */}
-                      </FormControl>
-                    </VStack>
-                  </VStack>
-                </Modal.Body>
-              ))}
-            {(date?.early && !date?.earlyType) ||
-              (date?.early && date?.earlyType && (
-                <Modal.Body>
-                  <VStack
-                    w="95%"
-                    space={3}
-                    // pb={keyboardHeight}
-                  >
-                    <VStack w="100%" space={2}>
-                      <FormControl
-                      // isInvalid={formik.errors.leave_id}
-                      >
-                        <FormControl.Label>Early Type</FormControl.Label>
-                      </FormControl>
-                      <Select
-                        // mt={-3}
-                        // selectedValue={formik.values.leave_id}
-                        // onValueChange={(value) => formik.setFieldValue("leave_id", value)}
-                        borderRadius={15}
-                        borderWidth={1}
-                        variant="unstyled"
-                        // key="leave_id"
-                        placeholder="Select Early Type"
-                        dropdownIcon={<Icon as={<MaterialCommunityIcons name="chevron-down" />} size="lg" mr={2} />}
-                      >
-                        {earlyType.map((item) => {
-                          return <Select.Item label={item?.name} value={item?.id} key={item?.id} />;
-                        })}
-                      </Select>
-                      <FormControl
-                        mt={-2}
-                        // isInvalid={formik.errors.leave_id}
-                      >
-                        {/* <FormControl.ErrorMessage>{formik.errors.leave_id}</FormControl.ErrorMessage> */}
-                      </FormControl>
-                      <FormControl.Label>Reason</FormControl.Label>
+            {/* If employee Clock in late, require Late Report */}
+            {date?.late && date?.lateType && !date?.lateReason && (
+              <Modal.Body>
+                <VStack
+                  w="95%"
+                  space={3}
+                  // pb={keyboardHeight}
+                >
+                  <VStack w="100%" space={2}>
+                    <FormControl isInvalid={formik.errors.late_type}>
+                      <FormControl.Label>Late Type</FormControl.Label>
+                    </FormControl>
+                    <Select
+                      onValueChange={(value) => formik.setFieldValue("late_type", value)}
+                      borderRadius={15}
+                      borderWidth={1}
+                      variant="unstyled"
+                      key="late_type"
+                      placeholder="Select Late Type"
+                      dropdownIcon={<Icon as={<MaterialCommunityIcons name="chevron-down" />} size="lg" mr={2} />}
+                    >
+                      {lateType.map((item) => {
+                        return <Select.Item label={item?.name} value={item?.name} key={item?.id} />;
+                      })}
+                    </Select>
+                    <FormControl mt={-2} isInvalid={formik.errors.late_type}>
+                      <FormControl.ErrorMessage>{formik.errors.late_type}</FormControl.ErrorMessage>
+                    </FormControl>
 
-                      <FormControl
-                      // isInvalid={formik.errors.password}
-                      >
-                        <Input
-                          variant="outline"
-                          placeholder="Enter your reason"
-                          // value={formik.values.password}
-                          // onChangeText={(value) => formik.setFieldValue("password", value)}
-                        />
-                        {/* <FormControl.ErrorMessage>{formik.errors.password}</FormControl.ErrorMessage> */}
-                      </FormControl>
-                    </VStack>
+                    <FormControl.Label>Reason</FormControl.Label>
+                    <FormControl isInvalid={formik.errors.late_reason}>
+                      <Input
+                        variant="outline"
+                        placeholder="Enter your reason"
+                        value={formik.values.late_reason}
+                        onChangeText={(value) => formik.setFieldValue("late_reason", value)}
+                      />
+                      <FormControl.ErrorMessage>{formik.errors.late_reason}</FormControl.ErrorMessage>
+                    </FormControl>
                   </VStack>
-                </Modal.Body>
-              ))}
-            {date?.lateReason || date?.earlyReason ? (
+                </VStack>
+              </Modal.Body>
+            )}
+
+            {/* If employee Clock out early, require Early Report */}
+            {date?.early && date?.earlyType && !date?.earlyReason && (
+              <Modal.Body>
+                <VStack
+                  w="95%"
+                  space={3}
+                  // pb={keyboardHeight}
+                >
+                  <VStack w="100%" space={2}>
+                    <FormControl isInvalid={formik.errors.early_type}>
+                      <FormControl.Label>Early Type</FormControl.Label>
+                    </FormControl>
+                    <Select
+                      onValueChange={(value) => formik.setFieldValue("early_type", value)}
+                      borderRadius={15}
+                      borderWidth={1}
+                      variant="unstyled"
+                      key="early_type"
+                      placeholder="Select Early Type"
+                      dropdownIcon={<Icon as={<MaterialCommunityIcons name="chevron-down" />} size="lg" mr={2} />}
+                    >
+                      {earlyType.map((item) => {
+                        return <Select.Item label={item?.name} value={item?.name} key={item?.id} />;
+                      })}
+                    </Select>
+                    <FormControl mt={-2} isInvalid={formik.errors.early_type}>
+                      <FormControl.ErrorMessage>{formik.errors.early_type}</FormControl.ErrorMessage>
+                    </FormControl>
+                    <FormControl.Label>Reason</FormControl.Label>
+
+                    <FormControl isInvalid={formik.errors.early_reason}>
+                      <Input
+                        variant="outline"
+                        placeholder="Enter your reason"
+                        value={formik.values.early_reason}
+                        onChangeText={(value) => formik.setFieldValue("early_reason", value)}
+                      />
+                      <FormControl.ErrorMessage>{formik.errors.early_reason}</FormControl.ErrorMessage>
+                    </FormControl>
+                  </VStack>
+                </VStack>
+              </Modal.Body>
+            )}
+
+            {/* If report submitted either Late or Early */}
+            {(date?.lateReason && !date?.earlyReason) || (!date?.lateReason && date?.earlyReason) ? (
               <Modal.Body>
                 <VStack w="95%" space={3}>
                   <VStack w="100%" space={2}>
@@ -306,23 +345,64 @@ const AttendanceCalendar = ({ attendance, onMonthChange }) => {
                   </VStack>
                 </VStack>
               </Modal.Body>
+            ) : /**
+             * If report submitted  Late and Early
+             */
+            date?.lateType && date?.lateReason && date?.earlyType && date?.earlyReason ? (
+              <Modal.Body>
+                <VStack w="95%" space={3}>
+                  <VStack w="100%" space={2}>
+                    <FormControl>
+                      <FormControl.Label>Late Type</FormControl.Label>
+                      <Text>{date?.lateType}</Text>
+                    </FormControl>
+                    <FormControl>
+                      <FormControl.Label>Reason</FormControl.Label>
+                      <Text>{date?.lateReason}</Text>
+                    </FormControl>
+                    <FormControl>
+                      <FormControl.Label>Status</FormControl.Label>
+                      <Text>{date?.lateStatus}</Text>
+                    </FormControl>
+                    <FormControl>
+                      <FormControl.Label>Early Type</FormControl.Label>
+                      <Text>{date?.earlyType}</Text>
+                    </FormControl>
+                    <FormControl>
+                      <FormControl.Label>Reason</FormControl.Label>
+                      <Text>{date?.earlyReason}</Text>
+                    </FormControl>
+                    <FormControl>
+                      <FormControl.Label>Status</FormControl.Label>
+                      <Text>{date?.earlyStatus}</Text>
+                    </FormControl>
+                  </VStack>
+                </VStack>
+              </Modal.Body>
             ) : null}
 
             <Modal.Footer>
-              {date?.lateType || date?.earlyType ? (
+              {(date?.lateType && date?.lateReason) || (date?.earlyType && date?.earlyReason) ? (
                 <FormButton color="red.800" size="sm" variant="outline" onPress={toggleReport}>
                   <Text color="white">Close</Text>
                 </FormButton>
               ) : (
                 <>
-                  <FormButton color="red.800" size="sm" variant="outline" onPress={toggleReport}>
+                  <FormButton
+                    color="red.800"
+                    size="sm"
+                    variant="outline"
+                    onPress={() => {
+                      toggleReport(formik.resetForm);
+                    }}
+                  >
                     <Text color="white">Cancel</Text>
                   </FormButton>
                   <FormButton
                     size="sm"
                     variant="solid"
-                    // isSubmitting={formik.isSubmitting}
-                    // onPress={formik.handleSubmit}
+                    isSubmitting={formik.isSubmitting}
+                    onPress={formik.handleSubmit}
                   >
                     <Text color="white">Save</Text>
                   </FormButton>
