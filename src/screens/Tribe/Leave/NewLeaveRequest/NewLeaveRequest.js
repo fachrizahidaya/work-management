8810433a0c160d9ad7bbedbcbb59e258c8d38d91
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-10-13 15:40:46 +0700

    fix: new leave request

--- src/screens/Tribe/Leave/NewLeaveRequest/NewLeaveRequest.js
+++ src/screens/Tribe/Leave/NewLeaveRequest/NewLeaveRequest.js
@@ -198,7 +198,7 @@ const NewLeaveRequest = ({ route }) => {
       <Box w={width} height={height} bgColor="white" p={3}>
         <PageHeader title="New Leave Request" onPress={() => navigation.navigate("Feed")} />
 
-        <Flex alignItems="center" justifyContent="center" gap={3} flexDir="row" my={5}>
+        <Flex alignItems="center" justifyContent="center" gap={3} flexDir="row" my={3}>
           {personalLeaveData.map((item) => {
             return (
               <Box key={item.id} alignItems="center" justifyContent="center" gap={2}>
@@ -230,64 +230,6 @@ const NewLeaveRequest = ({ route }) => {
           onChangeStartDate={onChangeStartDate}
           selectedGenerateType={selectedGenerateType}
         />
-        {/* <Flex gap={13}>
-          <FormControl isInvalid={formik.errors.leave_id}>
-            <FormControl.Label>Leave Type</FormControl.Label>
-          </FormControl>
-          <Select
-            mt={-3}
-            selectedValue={formik.values.leave_id}
-            onValueChange={(value) => formik.setFieldValue("leave_id", value)}
-            borderRadius={15}
-            borderWidth={1}
-            variant="unstyled"
-            key="leave_id"
-            accessibilityLabel="Select Leave type"
-            placeholder="Select Leave type"
-            dropdownIcon={<Icon as={<MaterialCommunityIcons name="chevron-down" />} size="lg" mr={2} />}
-          >
-            {leaveType?.data.map((item) => {
-              return <Select.Item label={item?.name} value={item?.id} key={item?.id} />;
-            })}
-          </Select>
-          <FormControl mt={-2} isInvalid={formik.errors.leave_id}>
-            <FormControl.ErrorMessage>{formik.errors.leave_id}</FormControl.ErrorMessage>
-          </FormControl>
-
-          <FormControl isInvalid={formik.errors.reason}>
-            <FormControl.Label>Purpose of Leaving</FormControl.Label>
-            <TextArea
-              value={formik.values.reason}
-              h={100}
-              onChangeText={(value) => formik.setFieldValue("reason", value)}
-              placeholder="Input purpose"
-            />
-            <FormControl.ErrorMessage>{formik.errors.reason}</FormControl.ErrorMessage>
-          </FormControl>
-
-          <FormControl isInvalid={formik.errors.begin_date}>
-            <FormControl.Label>Start Date</FormControl.Label>
-            <CustomDateTimePicker
-              defaultValue={formik.values.begin_date}
-              onChange={onChangeStartDate}
-              disabled={!formik.values.leave_id}
-            />
-            <FormControl.ErrorMessage>{formik.errors.begin_date}</FormControl.ErrorMessage>
-          </FormControl>
-          <FormControl isInvalid={formik.errors.end_date}>
-            <FormControl.Label>End Date</FormControl.Label>
-            <CustomDateTimePicker
-              defaultValue={formik.values.end_date}
-              onChange={onChangeEndDate}
-              disabled={!formik.values.leave_id || !selectedGenerateType}
-            />
-            <FormControl.ErrorMessage>{formik.errors.end_date}</FormControl.ErrorMessage>
-          </FormControl>
-
-          <FormButton isSubmitting={formik.isSubmitting} onPress={formik.handleSubmit}>
-            <Text color="white">Submit</Text>
-          </FormButton>
-        </Flex> */}
       </Box>
     </Box>
   );
