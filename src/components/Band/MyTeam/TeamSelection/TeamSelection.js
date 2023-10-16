0ebb1328d4801be0f0db09c2a1a0ccfb731f03d7
jrmygp <fat.snorlax17@gmail.com>
2023-10-16 13:39:47 +0700

    Ref : Change my team flow (maybe need performance enchancement)

--- src/components/Band/MyTeam/TeamSelection/TeamSelection.js
+++ src/components/Band/MyTeam/TeamSelection/TeamSelection.js
@@ -1,21 +1,22 @@
-import React from "react";
+import React, { memo } from "react";
 
 import { Icon, Select } from "native-base";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-const TeamSelection = ({ onChange, selectedTeam, teams }) => {
+const TeamSelection = ({ onChange, selectedTeamId, teams }) => {
   return (
     <Select
-      selectedValue={selectedTeam?.name || ""}
+      selectedValue={selectedTeamId}
       dropdownIcon={<Icon as={<MaterialCommunityIcons name="chevron-down" />} size="lg" mr={2} />}
+      onValueChange={(value) => onChange(value)}
     >
-      <Select.Item value={""} label="Select Team" onPress={() => onChange({})} />
+      <Select.Item value={0} label="Select Team" />
       {teams.length > 0 &&
         teams.map((team) => {
-          return <Select.Item key={team.id} value={team.name} label={team.name} onPress={() => onChange(team)} />;
+          return <Select.Item key={team.id} value={team.id} label={team.name} />;
         })}
     </Select>
   );
 };
 
-export default TeamSelection;
+export default memo(TeamSelection);
