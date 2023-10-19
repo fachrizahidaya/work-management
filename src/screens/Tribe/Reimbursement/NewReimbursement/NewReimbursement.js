a18f8149795772f9f4ebb8b9551e353778207165
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-10-19 17:46:12 +0700

    feat: update name profile screen fix: confirmation return

--- src/screens/Tribe/Reimbursement/NewReimbursement/NewReimbursement.js
+++ src/screens/Tribe/Reimbursement/NewReimbursement/NewReimbursement.js
@@ -1,3 +1,4 @@
+import { useFormik } from "formik";
 import { useNavigation } from "@react-navigation/native";
 
 import { Dimensions } from "react-native";
@@ -5,19 +6,51 @@ import { Box, Text } from "native-base";
 
 import PageHeader from "../../../../components/shared/PageHeader";
 import NewReimbursementForm from "../../../../components/Tribe/Reimbursement/NewReimbursementForm";
+import { useDisclosure } from "../../../../hooks/useDisclosure";
+import ReturnConfirmationModal from "../../../../components/shared/ReturnConfirmationModal";
 
 const NewReimbursement = ({ route }) => {
   const { width, height } = Dimensions.get("window");
 
   const { onClose } = route.params;
 
+  const { isOpen: returnModalIsOpen, toggle: toggleReturnModal } = useDisclosure(false);
+
   const navigation = useNavigation();
+
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      title: "",
+      description: "",
+      total: "",
+      date: "",
+    },
+  });
 
   return (
     <Box position="absolute" zIndex={3}>
       <Box w={width} height={height} bgColor="#FFFFFF" p={3}>
-        <PageHeader title="New Reimbursement" onPress={() => navigation.navigate("Feed")} />
-        <NewReimbursementForm />
+        <PageHeader
+          title="New Reimbursement"
+          onPress={
+            formik.values.title || formik.values.description || formik.values.total || formik.values.date
+              ? toggleReturnModal
+              : () => navigation.navigate("Feed")
+          }
+        />
+
+        <ReturnConfirmationModal
+          isOpen={returnModalIsOpen}
+          toggle={toggleReturnModal}
+          onPress={() => {
+            toggleReturnModal();
+            navigation.navigate("Feed");
+          }}
+          description="If you return, It will be discarded"
+        />
+
+        <NewReimbursementForm formik={formik} />
       </Box>
     </Box>
   );
