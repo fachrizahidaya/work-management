cbc9b22e4aa7db55a072d0708a2f5b992e0abfa7
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-10-27 09:22:43 +0700

    fix: toast, employee profile

--- src/components/shared/CalendarWithSlider.js
+++ src/components/shared/CalendarWithSlider.js
@@ -31,7 +31,7 @@ const CalendarWithSlider = ({ items }) => {
 
   const [selectedDate, setSelectedDate] = useState(dayjs().format("YYYY-MM-DD"));
   const handleDayPress = (day) => {
-    setSelectedDate(day.dateString);
+    setSelectedDate(day);
   };
 
   const [isMonthVisible, setIsMonthVisible] = useState(true);
@@ -70,10 +70,12 @@ const CalendarWithSlider = ({ items }) => {
             </>
           );
         }}
-        onDayPress={handleDayPress}
+        onDayPress={() => {
+          handleDayPress();
+        }}
+
         // onCalendarToggled={toggleMonthVisibility}
       />
-      {/* </ScrollView> */}
     </>
   );
 };
