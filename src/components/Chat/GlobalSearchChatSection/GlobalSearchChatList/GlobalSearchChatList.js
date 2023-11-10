07686b7a2ed67da84a1441c2f16d36e10e5769c2
jrmygp <fat.snorlax17@gmail.com>
2023-11-10 18:09:10 +0700

    Feat : redirect on press GlobalSearchChatList

--- src/components/Chat/GlobalSearchChatSection/GlobalSearchChatList/GlobalSearchChatList.js
+++ src/components/Chat/GlobalSearchChatSection/GlobalSearchChatList/GlobalSearchChatList.js
@@ -1,14 +1,16 @@
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 import { useSelector } from "react-redux";
 
 import RenderHtml from "react-native-render-html";
 import { Box, Flex, HStack, Text, VStack } from "native-base";
-import { Dimensions } from "react-native";
+import { Dimensions, TouchableOpacity } from "react-native";
 
 import ChatTimeStamp from "../../ChatTimeStamp/ChatTimeStamp";
 
 const GlobalSearchChatList = ({ chat, searchKeyword, group }) => {
+  const navigation = useNavigation();
   const { width } = Dimensions.get("screen");
   const userSelector = useSelector((state) => state.auth);
 
@@ -22,51 +24,70 @@ const GlobalSearchChatList = ({ chat, searchKeyword, group }) => {
   };
 
   return (
-    <Flex p={4} borderBottomWidth={1} borderColor="#E8E9EB">
-      {group ? (
-        <>
-          <HStack justifyContent="space-between">
-            <Text fontSize={16} fontWeight={600}>
-              {group.name}
-            </Text>
-
-            <ChatTimeStamp time={chat.created_time} timestamp={chat.created_at} />
-          </HStack>
-          <HStack space={1}>
-            <Text>{userSelector.id === chat?.user?.id ? "You: " : `${chat?.user?.name}: `}</Text>
-
-            <Box flex={1} style={{ marginTop: 3 }}>
-              <RenderHtml
-                contentWidth={width}
-                source={{
-                  html: renderChat(),
-                }}
-              />
-            </Box>
-          </HStack>
-        </>
-      ) : (
-        <>
-          <VStack>
+    <TouchableOpacity
+      onPress={() => {
+        if (chat.group) {
+          navigation.navigate("Chat Room", {
+            name: chat.group.name,
+            userId: chat.group.id,
+            image: chat.group.image,
+            type: "group",
+          });
+        } else {
+          navigation.navigate("Chat Room", {
+            name: chat.user.name,
+            userId: chat.user.id,
+            image: chat.user.image,
+          });
+        }
+      }}
+    >
+      <Flex p={4} borderBottomWidth={1} borderColor="#E8E9EB">
+        {group ? (
+          <>
             <HStack justifyContent="space-between">
               <Text fontSize={16} fontWeight={600}>
-                {chat?.user?.name}
+                {group.name}
               </Text>
 
               <ChatTimeStamp time={chat.created_time} timestamp={chat.created_at} />
             </HStack>
-            <Box flex={1} style={{ marginTop: 3 }}>
-              <RenderHtml
-                contentWidth={width}
-                source={{
-                  html: renderChat(),
-                }}
-              />
-            </Box>
-          </VStack>
-        </>
-      )}
-    </Flex>
+            <HStack space={1}>
+              <Text>{userSelector.id === chat?.user?.id ? "You: " : `${chat?.user?.name}: `}</Text>
+
+              <Box flex={1} style={{ marginTop: 3 }}>
+                <RenderHtml
+                  contentWidth={width}
+                  source={{
+                    html: renderChat(),
+                  }}
+                />
+              </Box>
+            </HStack>
+          </>
+        ) : (
+          <>
+            <VStack>
+              <HStack justifyContent="space-between">
+                <Text fontSize={16} fontWeight={600}>
+                  {chat?.user?.name}
+                </Text>
+
+                <ChatTimeStamp time={chat.created_time} timestamp={chat.created_at} />
+              </HStack>
+              <Box flex={1} style={{ marginTop: 3 }}>
+                <RenderHtml
+                  contentWidth={width}
+                  source={{
+                    html: renderChat(),
+                  }}
+                />
+              </Box>
+            </VStack>
+          </>
+        )}
+      </Flex>
+    </TouchableOpacity>
   );
 };
 
