a1474caaf74570c706196dd146fc152fa3a15ab8
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-11-30 10:13:00 +0700

    fix: UI for ChatBubble,  ChatReply

--- src/components/Chat/ChatInput/ChatReplyPreview.js
+++ src/components/Chat/ChatInput/ChatReplyPreview.js
@@ -22,39 +22,36 @@ const ChatReplyPreview = ({ messageToReply, setMessageToReply, type }) => {
 
   return (
     messageToReply && (
-      <Flex flexDirection="row" alignItems="center" py={3} px={4} justifyContent="space-between">
-        <Flex
-          borderLeftColor="#37b4ea"
-          borderLeftWidth={5}
-          borderRadius={5}
-          px={2}
-          py={2}
-          gap={2}
-          width={300}
-          backgroundColor="#f1f1f1"
-          flexDirection="row"
-          alignItems="center"
-        >
-          <Flex width={mimeTypeInfo?.file_type === "image" ? 200 : null}>
-            <Text fontSize={12} fontWeight={700} color="#000000">
-              {messageToReply?.from_user_id === loggedInUser.id ? "You" : messageToReply?.user?.name}
-            </Text>
-            <ChatReplyPreviewMessage
-              message={messageToReply}
-              myMessage={messageToReply?.from_user_id === loggedInUser?.id}
-              type={type}
-            />
-          </Flex>
-          {mimeTypeInfo?.file_type === "image" && (
-            <Image
-              source={{ uri: `${process.env.EXPO_PUBLIC_API}/image/${messageToReply?.file_path}` }}
-              alt="Attachment Preview"
-              width={10}
-              height={10}
-              resizeMode="contain"
-            />
-          )}
+      <Flex
+        backgroundColor="#17668814"
+        flexDirection="row"
+        alignItems="center"
+        py={3}
+        px={4}
+        justifyContent="space-between"
+        borderLeftWidth={10}
+        borderLeftColor="#176688"
+      >
+        <Flex width={mimeTypeInfo?.file_type === "image" ? 200 : null}>
+          <Text fontSize={12} fontWeight={700} color="#176688">
+            {messageToReply?.from_user_id === loggedInUser.id ? "You" : messageToReply?.user?.name}
+          </Text>
+          <ChatReplyPreviewMessage
+            message={messageToReply}
+            myMessage={messageToReply?.from_user_id === loggedInUser?.id}
+            type={type}
+          />
         </Flex>
+        {mimeTypeInfo?.file_type === "image" && (
+          <Image
+            source={{ uri: `${process.env.EXPO_PUBLIC_API}/image/${messageToReply?.file_path}` }}
+            alt="Attachment Preview"
+            width={10}
+            height={10}
+            resizeMode="contain"
+          />
+        )}
+
         <Icon onPress={() => setMessageToReply(null)} as={<MaterialCommunityIcons name="close" />} size={5} />
       </Flex>
     )
