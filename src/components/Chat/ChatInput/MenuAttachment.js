ba935d10da68aed3989e2dad6abdc1b77ab3ca01
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-11-30 10:13:29 +0700

    fix: new UI MenuAttachment

--- src/components/Chat/ChatInput/MenuAttachment.js
+++ src/components/Chat/ChatInput/MenuAttachment.js
@@ -4,17 +4,38 @@ import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityI
 const MenuAttachment = ({ isOpen, onClose }) => {
   return (
     <Actionsheet isOpen={isOpen} onClose={onClose}>
-      <Actionsheet.Content>
-        <Actionsheet.Item borderWidth={1} justifyContent="space-between">
-          <Text fontSize={16} fontWeight={400}>
-            Document
-          </Text>
-          <Icon as={<MaterialCommunityIcons name="file-document-outline" />} color="#1E4AB9" />
-          {/* <Flex justifyContent="space-between" alignItems="center" flexDirection="row">
-          </Flex> */}
+      <Actionsheet.Content gap={1}>
+        <Actionsheet.Item backgroundColor="#F5F5F5">
+          <Flex width={350} justifyContent="space-between" alignItems="center" flexDirection="row">
+            <Text fontSize={16} fontWeight={400}>
+              Document
+            </Text>
+            <Icon as={<MaterialCommunityIcons name="file-document-outline" />} color="#1E4AB9" />
+          </Flex>
         </Actionsheet.Item>
-        <Actionsheet.Item>Photos</Actionsheet.Item>
-        <Actionsheet.Item>Project/Task</Actionsheet.Item>
+        <Actionsheet.Item backgroundColor="#F5F5F5">
+          <Flex width={350} justifyContent="space-between" alignItems="center" flexDirection="row">
+            <Text fontSize={16} fontWeight={400}>
+              Photos
+            </Text>
+            <Icon as={<MaterialCommunityIcons name="image-multiple-outline" />} color="#39B326" />
+          </Flex>
+        </Actionsheet.Item>
+        <Actionsheet.Item backgroundColor="#F5F5F5">
+          <Flex width={350} justifyContent="space-between" alignItems="center" flexDirection="row">
+            <Text fontSize={16} fontWeight={400}>
+              Project/Task
+            </Text>
+            <Icon as={<MaterialCommunityIcons name="circle-slice-2" />} color="#EB0E29" />
+          </Flex>
+        </Actionsheet.Item>
+        <Actionsheet.Item mt={1} backgroundColor="#F5F5F5">
+          <Flex width={350} justifyContent="center" alignItems="center">
+            <Text fontSize={16} fontWeight={400} color="#176688">
+              Cancel
+            </Text>
+          </Flex>
+        </Actionsheet.Item>
       </Actionsheet.Content>
     </Actionsheet>
   );
