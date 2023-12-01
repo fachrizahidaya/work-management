f482f4d936c2282272b3f50dc0d6f1e8854ecf1a
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-12-01 16:15:47 +0700

    fix: UI ChatList

--- src/components/Chat/ChatInput/MenuAttachment.js
+++ src/components/Chat/ChatInput/MenuAttachment.js
@@ -1,35 +1,54 @@
 import { Actionsheet, Flex, Icon, Text } from "native-base";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-const MenuAttachment = ({ isOpen, onClose }) => {
+const MenuAttachment = ({ isOpen, onClose, selectFile, pickImageHandler, selectBandHandler }) => {
+  const attachmentOptions = [
+    {
+      icon: "file-document-outline",
+      name: "Document",
+      color: "#1E4AB9",
+      onPress: () => {
+        selectFile();
+        onClose();
+      },
+    },
+    {
+      icon: "image-multiple-outline",
+      name: "Photo",
+      color: "#39B326",
+      onPress: () => {
+        pickImageHandler();
+        onClose();
+      },
+    },
+    {
+      icon: "circle-slice-2",
+      name: "Project/Task",
+      color: "#EB0E29",
+      onPress: () => {
+        selectBandHandler("project");
+        onClose();
+      },
+    },
+  ];
+
   return (
     <Actionsheet isOpen={isOpen} onClose={onClose}>
       <Actionsheet.Content gap={1}>
-        <Actionsheet.Item backgroundColor="#F5F5F5">
-          <Flex width={350} justifyContent="space-between" alignItems="center" flexDirection="row">
-            <Text fontSize={16} fontWeight={400}>
-              Document
-            </Text>
-            <Icon as={<MaterialCommunityIcons name="file-document-outline" />} color="#1E4AB9" />
-          </Flex>
-        </Actionsheet.Item>
-        <Actionsheet.Item backgroundColor="#F5F5F5">
-          <Flex width={350} justifyContent="space-between" alignItems="center" flexDirection="row">
-            <Text fontSize={16} fontWeight={400}>
-              Photos
-            </Text>
-            <Icon as={<MaterialCommunityIcons name="image-multiple-outline" />} color="#39B326" />
-          </Flex>
-        </Actionsheet.Item>
-        <Actionsheet.Item backgroundColor="#F5F5F5">
-          <Flex width={350} justifyContent="space-between" alignItems="center" flexDirection="row">
-            <Text fontSize={16} fontWeight={400}>
-              Project/Task
-            </Text>
-            <Icon as={<MaterialCommunityIcons name="circle-slice-2" />} color="#EB0E29" />
-          </Flex>
-        </Actionsheet.Item>
-        <Actionsheet.Item mt={1} backgroundColor="#F5F5F5">
+        {attachmentOptions.map((option, index) => {
+          return (
+            <Actionsheet.Item key={index} onPress={option.onPress} backgroundColor="#F5F5F5">
+              <Flex width={350} justifyContent="space-between" alignItems="center" flexDirection="row">
+                <Text fontSize={16} fontWeight={400}>
+                  {option.name}
+                </Text>
+                <Icon as={<MaterialCommunityIcons name={option.icon} />} color={option.color} />
+              </Flex>
+            </Actionsheet.Item>
+          );
+        })}
+
+        <Actionsheet.Item onPress={onClose} mt={1} backgroundColor="#F5F5F5">
           <Flex width={350} justifyContent="center" alignItems="center">
             <Text fontSize={16} fontWeight={400} color="#176688">
               Cancel
