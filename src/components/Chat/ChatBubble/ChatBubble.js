4de35471c01be6fc9dfcba01e5edfef2004893d1
Fachriza Hidaya <fachriza.theblues@gmail.com>
2023-12-08 23:33:19 +0700

    fix: swipe ChatBubble, swipe ContactListItem

--- src/components/Chat/ChatBubble/ChatBubble.js
+++ src/components/Chat/ChatBubble/ChatBubble.js
@@ -15,7 +15,6 @@ import { CopyToClipboard } from "../../shared/CopyToClipboard";
 import FileAttachmentBubble from "./FileAttachmentBubble";
 import BandAttachmentBubble from "./BandAttachmentBubble";
 import ChatReplyInfo from "./ChatReplyInfo";
-import { useRef } from "react";
 
 const ChatBubble = ({
   chat,
