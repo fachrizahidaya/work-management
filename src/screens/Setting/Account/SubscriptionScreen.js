8dbf5faf4d7660df4911b80f9653ead6bd9e2293
jrmygp <fat.snorlax17@gmail.com>
2024-01-01 19:25:54 +0700

    Feat : Setting screens migrated

--- src/screens/Setting/Account/SubscriptionScreen.js
+++ src/screens/Setting/Account/SubscriptionScreen.js
@@ -1,35 +1,19 @@
 import { useNavigation } from "@react-navigation/native";
 
-import { Box, Flex, Icon, Pressable, Text } from "native-base";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import PageHeader from "../../../components/shared/PageHeader";
 
-const SubscriptionScreen = ({ route }) => {
-  const { profile, editProfileHandler } = route.params;
+const SubscriptionScreen = () => {
   const navigation = useNavigation();
 
   return (
     <SafeAreaView style={styles.container}>
-      <Flex flexDir="row" alignItems="center" justifyContent="space-between" bgColor="#FFFFFF" py={14} px={15}>
-        <Flex flexDir="row" gap={1}>
-          <Pressable
-            onPress={() =>
-              navigation.navigate("Account Screen", { profile: profile, editProfileHandler: editProfileHandler })
-            }
-          >
-            <Icon as={<MaterialCommunityIcons name="keyboard-backspace" />} size="xl" color="#3F434A" />
-          </Pressable>
-          <Text fontSize={16}>Subscriptions</Text>
-        </Flex>
-      </Flex>
+      <View style={{ display: "flex", marginVertical: 15, paddingHorizontal: 16, gap: 24 }}>
+        <PageHeader title="Subscribtions" onPress={() => navigation.goBack()} />
 
-      <Flex my={3} gap={11} px={5}>
-        <Box width={250}>
-          <Text color="#3F434A">redirect to website</Text>
-        </Box>
-      </Flex>
+        <Text style={{ fontWeight: 500 }}>redirect to website</Text>
+      </View>
     </SafeAreaView>
   );
 };
