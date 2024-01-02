90cd062ea74a20d18ca8e17dbef82e52c4d452a6
jrmygp <fat.snorlax17@gmail.com>
2024-01-02 13:49:30 +0700

    UI : stack avatars for team's group in SettingScreen

--- src/screens/Setting/SettingScreen.js
+++ src/screens/Setting/SettingScreen.js
@@ -101,11 +101,28 @@ const SettingScreen = () => {
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.item}>
-              <View style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 2 }}>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 2,
+                }}
+              >
                 {team?.data?.length > 0 &&
                   (!teamIsLoading ? (
-                    team.data.map((item) => {
-                      return <AvatarPlaceholder key={item.id} image={item.image} name={item.name} size="xs" />;
+                    team.data.map((item, index) => {
+                      return (
+                        <AvatarPlaceholder
+                          key={item.id}
+                          image={item.image}
+                          name={item.name}
+                          style={{
+                            marginLeft: index === 0 ? 0 : -12,
+                          }}
+                          size="xs"
+                        />
+                      );
                     })
                   ) : (
                     // <Skeleton h={35} />
