d8fdee1bd56238f3b73dce3ef1747ebd41d1b4ad
Fachriza Hidaya <fachriza.theblues@gmail.com>
2024-01-09 11:17:40 +0700

    fix: reimbursement

--- src/screens/Tribe/Reimbursement/NewReimbursement/NewReimbursement.js
+++ src/screens/Tribe/Reimbursement/NewReimbursement/NewReimbursement.js
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import { useFormik } from "formik";
 import { useNavigation } from "@react-navigation/native";
+import * as DocumentPicker from "expo-document-picker";
 
-import { Dimensions, View } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
+import Toast from "react-native-root-toast";
 
 import PageHeader from "../../../../components/shared/PageHeader";
 import NewReimbursementForm from "../../../../components/Tribe/Reimbursement/NewReimbursementForm";
 import { useDisclosure } from "../../../../hooks/useDisclosure";
 import ReturnConfirmationModal from "../../../../components/shared/ReturnConfirmationModal";
+import { ErrorToastProps } from "../../../../components/shared/CustomStylings";
 
-const NewReimbursement = ({ route }) => {
+const NewReimbursement = () => {
+  const [fileAttachment, setFileAttachment] = useState(null);
   const { width, height } = Dimensions.get("window");
-
-  const { onClose } = route.params;
 
   const { isOpen: returnModalIsOpen, toggle: toggleReturnModal } = useDisclosure(false);
 
@@ -27,9 +30,37 @@ const NewReimbursement = ({ route }) => {
     },
   });
 
+  /**
+   * Select file handler
+   */
+  const selectFile = async () => {
+    try {
+      const result = await DocumentPicker.getDocumentAsync({
+        copyToCacheDirectory: false,
+      });
+
+      // Check if there is selected file
+      if (result) {
+        if (result.assets[0].size < 3000001) {
+          setFileAttachment({
+            name: result.assets[0].name,
+            size: result.assets[0].size,
+            type: result.assets[0].mimeType,
+            uri: result.assets[0].uri,
+            webkitRelativePath: "",
+          });
+        } else {
+          Toast.show("Max file size is 3MB", ErrorToastProps);
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <View style={{ position: "absolute", zIndex: 3 }}>
-      <View style={{ width: width, height: height, backgroundColor: "#FFFFFF", padding: 5 }}>
+    <View>
+      <View style={{ ...styles.container, width: width, height: height }}>
         <PageHeader
           title="New Reimbursement"
           onPress={
@@ -49,10 +80,17 @@ const NewReimbursement = ({ route }) => {
           description="If you return, It will be discarded"
         />
 
-        <NewReimbursementForm formik={formik} />
+        <NewReimbursementForm formik={formik} onSelectFile={selectFile} fileAttachment={fileAttachment} />
       </View>
     </View>
   );
 };
 
 export default NewReimbursement;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#FFFFFF",
+    padding: 15,
+  },
+});
