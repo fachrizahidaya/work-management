3685ff86649e8d4d8686251791a8bdb774d51ee3
Fachriza Hidaya <fachriza.theblues@gmail.com>
2024-01-10 11:38:12 +0700

    fix: prevent conflict

--- src/components/shared/Forms/Select.js
+++ src/components/shared/Forms/Select.js
@@ -6,29 +6,31 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { TextProps } from "../CustomStylings";
 
-const Select = ({ placeHolder, items, value, onChange, title, formik, fieldName = "", defaultValue, otherValue }) => {
+const Select = ({ placeHolder, items = [], value, onChange, title, formik, fieldName = "", hasParentSheet }) => {
   const onPressValue = (value) => {
     onChange(value);
     SheetManager.hide("select-sheet");
   };
+
+  const valueToPrint = items.find((item) => item.value === value);
+
   return (
     <View style={styles.wrapper}>
       {title && <Text style={[{ marginBottom: 9 }, TextProps]}>{title}</Text>}
 
       <TouchableOpacity
         style={styles.select}
-        onPress={() =>
+        onPress={async () => {
+          hasParentSheet && (await SheetManager.hide("form-sheet"));
           SheetManager.show("select-sheet", {
             payload: {
               children: items,
               onChange: onPressValue,
             },
-          })
-        }
+          });
+        }}
       >
-        <Text style={TextProps}>
-          {defaultValue ? defaultValue : otherValue ? otherValue : value ? value : placeHolder}
-        </Text>
+        <Text style={TextProps}>{valueToPrint?.label || placeHolder}</Text>
 
         <MaterialCommunityIcons name="chevron-down" style={styles.dropdownIcon} size={20} color="#3F434A" />
       </TouchableOpacity>
