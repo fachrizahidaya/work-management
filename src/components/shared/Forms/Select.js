de65a2be9351cee9a98480bb420d3979b284668b
Fachriza Hidaya <fachriza.theblues@gmail.com>
2024-01-10 11:35:15 +0700

    fix: select

--- src/components/shared/Forms/Select.js
+++ src/components/shared/Forms/Select.js
@@ -6,7 +6,7 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { TextProps } from "../CustomStylings";
 
-const Select = ({ placeHolder, items, value, onChange, title, formik, fieldName = "", defaultValue, value1 }) => {
+const Select = ({ placeHolder, items, value, onChange, title, formik, fieldName = "", defaultValue, otherValue }) => {
   const onPressValue = (value) => {
     onChange(value);
     SheetManager.hide("select-sheet");
@@ -26,7 +26,9 @@ const Select = ({ placeHolder, items, value, onChange, title, formik, fieldName 
           })
         }
       >
-        <Text style={TextProps}>{defaultValue ? defaultValue : value1 ? value1 : value ? value : placeHolder}</Text>
+        <Text style={TextProps}>
+          {defaultValue ? defaultValue : otherValue ? otherValue : value ? value : placeHolder}
+        </Text>
 
         <MaterialCommunityIcons name="chevron-down" style={styles.dropdownIcon} size={20} color="#3F434A" />
       </TouchableOpacity>
