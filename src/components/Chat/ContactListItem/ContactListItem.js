4328f919271b7dec0c9b137d29a925fff4edb5bd
Fachriza Hidaya <fachriza.theblues@gmail.com>
2024-01-19 13:08:05 +0700

    fix: contactListItem swipe

--- src/components/Chat/ContactListItem/ContactListItem.js
+++ src/components/Chat/ContactListItem/ContactListItem.js
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 
 import { Swipeable } from "react-native-gesture-handler";
 import RenderHtml from "react-native-render-html";
-import { StyleSheet, TouchableOpacity, View, Text, Pressable } from "react-native";
+import { StyleSheet, TouchableOpacity, View, Text, Pressable, Animated } from "react-native";
 
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
@@ -39,7 +39,9 @@ const ContactListItem = ({
   userSelector,
 }) => {
   const [selectedGroupMembers, setSelectedGroupMembers] = useState([]);
+  const [activeView, setActiveView] = useState(null);
   const swipeableRef = useRef(null);
+  console.log("a", activeView);
 
   const memberName = selectedGroupMembers.map((item) => {
     return item?.user?.name;
@@ -111,62 +113,56 @@ const ContactListItem = ({
 
   const renderLeftView = () => {
     return (
-      <View
+      <Pressable
+        onPress={() => {
+          if (swipeableRef.current) {
+            swipeableRef.current.close();
+          }
+          onPin(type, id, isPinned?.pin_chat ? "unpin" : "pin");
+        }}
         style={{
           backgroundColor: "#377893",
-          width: 400,
+          width: 80,
+          padding: 20,
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        <Pressable
-          onPress={() => {
-            if (swipeableRef.current) {
-              swipeableRef.current.close();
-            }
-            onPin(type, id, isPinned?.pin_chat ? "unpin" : "pin");
-          }}
-          style={{
-            padding: 20,
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "#377893",
-            width: 100,
-          }}
-        >
-          <MaterialIcons name="push-pin" color="#FFFFFF" style={{ transform: [{ rotate: "45deg" }] }} />
-          <Text style={{ color: "#FFFFFF" }}>{isPinned?.pin_chat ? "Unpin" : "Pin"}</Text>
-        </Pressable>
-      </View>
+        <MaterialIcons name="push-pin" color="#FFFFFF" style={{ transform: [{ rotate: "45deg" }] }} />
+        <Text style={{ color: "#FFFFFF" }}>{isPinned?.pin_chat ? "Unpin" : "Pin"}</Text>
+      </Pressable>
     );
   };
 
   const renderRightView = () => {
     return (
-      <View
+      <Pressable
+        onPress={() => {
+          if (swipeableRef.current) {
+            swipeableRef.current.close();
+          }
+          onSwipe(chat);
+        }}
         style={{
+          width: 100,
+          padding: 20,
+          alignItems: "center",
+          justifyContent: "center",
           backgroundColor: "#959595",
-          width: 400,
         }}
       >
-        <Pressable
-          onPress={() => {
-            if (swipeableRef.current) {
-              swipeableRef.current.close();
-            }
-            onSwipe(chat);
-          }}
-          style={{
-            marginLeft: 300,
-            padding: 20,
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "#959595",
-          }}
-        >
-          <MaterialIcons name="more-horiz" color="#FFFFFF" />
-          <Text style={{ color: "#FFFFFF" }}>More</Text>
-        </Pressable>
-      </View>
+        <MaterialIcons name="more-horiz" color="#FFFFFF" />
+        <Text style={{ color: "#FFFFFF" }}>More</Text>
+      </Pressable>
     );
+  };
+
+  const getBackgroundColor = () => {
+    if (activeView === "left") {
+      return "#377893";
+    } else if (activeView === "right") {
+      return "#959595";
+    }
   };
 
   /**
@@ -187,144 +183,154 @@ const ContactListItem = ({
 
   return (
     <View>
-      <Swipeable ref={swipeableRef} renderLeftActions={renderLeftView} renderRightActions={renderRightView}>
-        <TouchableOpacity
-          style={{ backgroundColor: "#FFFFFF" }}
-          activeOpacity={1}
-          onPress={() => {
-            navigation.navigate("Chat Room", {
-              userId: userId,
-              name: name,
-              roomId: id,
-              image: image,
-              position: position,
-              email: email,
-              type: type,
-              active_member: active_member,
-              isPinned: isPinned,
-            });
-          }}
-        >
-          <View style={styles.contactBox}>
-            <View style={{ flex: 1, flexDirection: "row", alignItems: "center", gap: 10 }}>
-              <AvatarPlaceholder name={name} image={image} size="md" isThumb={false} />
+      <Animated.View style={{ backgroundColor: getBackgroundColor() }}>
+        <Animated.View>
+          <Swipeable
+            ref={swipeableRef}
+            renderLeftActions={renderLeftView}
+            renderRightActions={renderRightView}
+            onSwipeableOpen={(direction = "left" || "right") => setActiveView(direction === "right" ? "right" : "left")}
+            onSwipeableClose={() => setActiveView(null)}
+          >
+            <TouchableOpacity
+              style={{ backgroundColor: "#FFFFFF" }}
+              activeOpacity={1}
+              onPress={() => {
+                navigation.navigate("Chat Room", {
+                  userId: userId,
+                  name: name,
+                  roomId: id,
+                  image: image,
+                  position: position,
+                  email: email,
+                  type: type,
+                  active_member: active_member,
+                  isPinned: isPinned,
+                });
+              }}
+            >
+              <View style={styles.contactBox}>
+                <View style={{ flex: 1, flexDirection: "row", alignItems: "center", gap: 10 }}>
+                  <AvatarPlaceholder name={name} image={image} size="md" isThumb={false} />
 
-              <View style={{ flex: 1 }}>
-                <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-                  {!searchKeyword ? (
-                    <Text style={[{ fontSize: 12, fontWeight: "500" }]}>{name}</Text>
-                  ) : (
-                    <RenderHtml
-                      contentWidth={400}
-                      source={{
-                        html: renderName(),
-                      }}
-                    />
-                  )}
-
-                  <View style={{ flexDirection: "row" }}>
-                    <ChatTimeStamp time={time} timestamp={timestamp} />
-                  </View>
-                </View>
-
-                <View
-                  style={{
-                    flexDirection: "row",
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <View style={{ flexDirection: "row", alignItems: "center", flex: 1 }}>
-                    {type === "group" && chat?.latest_message ? (
-                      <Text style={[{ fontSize: 12 }, TextProps]}>
-                        {userSelector?.name === chat?.latest_message?.user?.name
-                          ? "You"
-                          : chat?.latest_message?.user?.name}
-                        :{" "}
-                      </Text>
-                    ) : null}
-                    {!isDeleted ? (
-                      <>
-                        <View
-                          style={{
-                            flex: 1,
-                            flexDirection: "row",
-                            alignItems: "center",
-                            justifyContent: "space-between",
+                  <View style={{ flex: 1 }}>
+                    <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                      {!searchKeyword ? (
+                        <Text style={[{ fontSize: 12, fontWeight: "500" }]}>{name}</Text>
+                      ) : (
+                        <RenderHtml
+                          contentWidth={400}
+                          source={{
+                            html: renderName(),
                           }}
-                        >
-                          <View style={{ flexDirection: "row" }}>
-                            {message && (
-                              <Text style={[{ fontSize: 12 }, TextProps]}>
-                                {message.length > 20 ? message.slice(0, 20) + "..." : message}
-                              </Text>
-                            )}
-                            {!message && (project || task || fileName) && (
-                              <View style={{ flexDirection: "row", alignItems: "center", gap: 3 }}>
-                                <MaterialCommunityIcons name={generateIcon()} size={20} color="#3F434A" />
-                                <Text style={[{ fontSize: 12 }, TextProps]}>{generateAttachmentText()}</Text>
-                              </View>
-                            )}
-                          </View>
-                          {!!isRead && (
+                        />
+                      )}
+
+                      <View style={{ flexDirection: "row" }}>
+                        <ChatTimeStamp time={time} timestamp={timestamp} />
+                      </View>
+                    </View>
+
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <View style={{ flexDirection: "row", alignItems: "center", flex: 1 }}>
+                        {type === "group" && chat?.latest_message ? (
+                          <Text style={[{ fontSize: 12 }, TextProps]}>
+                            {userSelector?.name === chat?.latest_message?.user?.name
+                              ? "You"
+                              : chat?.latest_message?.user?.name}
+                            :{" "}
+                          </Text>
+                        ) : null}
+                        {!isDeleted ? (
+                          <>
                             <View
                               style={{
-                                height: 25,
-                                width: 25,
-                                backgroundColor: "#FD7972",
-                                borderRadius: 10,
-                                display: "flex",
+                                flex: 1,
+                                flexDirection: "row",
                                 alignItems: "center",
-                                justifyContent: "center",
+                                justifyContent: "space-between",
                               }}
                             >
-                              <Text style={{ fontSize: 12, textAlign: "center", color: "#FFFFFF" }}>
-                                {isRead > 20 ? "20+" : isRead}
-                              </Text>
+                              <View style={{ flexDirection: "row" }}>
+                                {message && (
+                                  <Text style={[{ fontSize: 12 }, TextProps]}>
+                                    {message.length > 20 ? message.slice(0, 20) + "..." : message}
+                                  </Text>
+                                )}
+                                {!message && (project || task || fileName) && (
+                                  <View style={{ flexDirection: "row", alignItems: "center", gap: 3 }}>
+                                    <MaterialCommunityIcons name={generateIcon()} size={20} color="#3F434A" />
+                                    <Text style={[{ fontSize: 12 }, TextProps]}>{generateAttachmentText()}</Text>
+                                  </View>
+                                )}
+                              </View>
+                              {!!isRead && (
+                                <View
+                                  style={{
+                                    height: 25,
+                                    width: 25,
+                                    backgroundColor: "#FD7972",
+                                    borderRadius: 10,
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                  }}
+                                >
+                                  <Text style={{ fontSize: 12, textAlign: "center", color: "#FFFFFF" }}>
+                                    {isRead > 20 ? "20+" : isRead}
+                                  </Text>
+                                </View>
+                              )}
                             </View>
-                          )}
-                        </View>
-                      </>
-                    ) : isDeleted && userSelector.id === latest?.user?.id ? (
-                      <View style={{ flexDirection: "row", alignItems: "center", gap: 3 }}>
+                          </>
+                        ) : isDeleted && userSelector.id === latest?.user?.id ? (
+                          <View style={{ flexDirection: "row", alignItems: "center", gap: 3 }}>
+                            <MaterialCommunityIcons
+                              name="block-helper"
+                              size={10}
+                              style={{ transform: [{ rotate: "90deg" }] }}
+                              color="#3F434A"
+                            />
+                            <Text style={[{ fontSize: 12, fontStyle: "italic", opacity: 0.5 }, TextProps]}>
+                              You deleted this message
+                            </Text>
+                          </View>
+                        ) : isDeleted && userSelector.id !== latest?.user?.id ? (
+                          <View style={{ flexDirection: "row", alignItems: "center", gap: 3 }}>
+                            <MaterialCommunityIcons
+                              name="block-helper"
+                              size={10}
+                              style={{ transform: [{ rotate: "90deg" }] }}
+                              color="#3F434A"
+                            />
+                            <Text style={[{ fontSize: 12, fontStyle: "italic", opacity: 0.5 }, TextProps]}>
+                              This message was deleted
+                            </Text>
+                          </View>
+                        ) : null}
+                      </View>
+                      {isPinned?.pin_chat ? (
                         <MaterialCommunityIcons
-                          name="block-helper"
-                          size={10}
-                          style={{ transform: [{ rotate: "90deg" }] }}
+                          name="pin"
+                          size={20}
+                          style={{ transform: [{ rotate: "45deg" }] }}
                           color="#3F434A"
                         />
-                        <Text style={[{ fontSize: 12, fontStyle: "italic", opacity: 0.5 }, TextProps]}>
-                          You deleted this message
-                        </Text>
-                      </View>
-                    ) : isDeleted && userSelector.id !== latest?.user?.id ? (
-                      <View style={{ flexDirection: "row", alignItems: "center", gap: 3 }}>
-                        <MaterialCommunityIcons
-                          name="block-helper"
-                          size={10}
-                          style={{ transform: [{ rotate: "90deg" }] }}
-                          color="#3F434A"
-                        />
-                        <Text style={[{ fontSize: 12, fontStyle: "italic", opacity: 0.5 }, TextProps]}>
-                          This message was deleted
-                        </Text>
-                      </View>
-                    ) : null}
+                      ) : null}
+                    </View>
                   </View>
-                  {isPinned?.pin_chat ? (
-                    <MaterialCommunityIcons
-                      name="pin"
-                      size={20}
-                      style={{ transform: [{ rotate: "45deg" }] }}
-                      color="#3F434A"
-                    />
-                  ) : null}
                 </View>
               </View>
-            </View>
-          </View>
-        </TouchableOpacity>
-      </Swipeable>
+            </TouchableOpacity>
+          </Swipeable>
+        </Animated.View>
+      </Animated.View>
     </View>
   );
 };
