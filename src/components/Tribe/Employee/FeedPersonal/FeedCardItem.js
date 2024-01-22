a660bbd5f88f3d0ca1a3bb08665e7fa110d99ae9
Fachriza Hidaya <fachriza.theblues@gmail.com>
2024-01-22 17:23:07 +0700

    fix: image size FeedCardITem

--- src/components/Tribe/Employee/FeedPersonal/FeedCardItem.js
+++ src/components/Tribe/Employee/FeedPersonal/FeedCardItem.js
@@ -276,7 +276,7 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     width: "100%",
-    height: 250,
+    height: 500,
     backgroundColor: "gray",
     resizeMode: "contain",
   },
