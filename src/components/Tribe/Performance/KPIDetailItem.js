ba85609832fe3e6b896dc4ab0b14e3e7985991fa
Fachriza Hidaya <fachriza.theblues@gmail.com>
2024-01-24 11:17:48 +0700

    feat: UI KPI

--- src/components/Tribe/Performance/KPIDetailItem.js
+++ src/components/Tribe/Performance/KPIDetailItem.js
@@ -1,8 +1,37 @@
 import React from "react";
-import { Text } from "react-native";
 
-const KPIDetailItem = () => {
-  return <Text>KPIDetailItem</Text>;
+import { Pressable, Text, View } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import { card } from "../../../styles/Card";
+import { TextProps } from "../../shared/CustomStylings";
+import { FlashList } from "@shopify/flash-list";
+
+const KPIDetailItem = ({ question, navigation }) => {
+  return (
+    <FlashList
+      data={question}
+      estimatedItemSize={50}
+      onEndReachedThreshold={0.1}
+      keyExtractor={(item, index) => index}
+      renderItem={({ item, index }) => (
+        <Pressable
+          style={{
+            ...card.card,
+            marginVertical: 5,
+            elevation: 1,
+            flexDirection: "column",
+            alignItems: "flex-start",
+            gap: 10,
+          }}
+          onPress={() => navigation.navigate("KPI Detail")}
+        >
+          <Text style={[TextProps]}>{item?.title}</Text>
+          <MaterialCommunityIcons name="calendar-month" size={15} style={{ opacity: 0.5 }} />
+        </Pressable>
+      )}
+    />
+  );
 };
 
 export default KPIDetailItem;
--- src/screens/Tribe/Performance/KPIScreen.js
+++ src/screens/Tribe/Performance/KPIScreen.js
@@ -1,10 +1,15 @@
 import React, { useCallback, useMemo, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import dayjs from "dayjs";
+
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { FlashList } from "@shopify/flash-list";
+
 import PageHeader from "../../../components/shared/PageHeader";
 import Tabs from "../../../components/shared/Tabs";
-import { FlashList } from "@shopify/flash-list";
 import KPIDetailItem from "../../../components/Tribe/Performance/KPIDetailItem";
+import { TextProps } from "../../../components/shared/CustomStylings";
 
 const KPIScreen = () => {
   const [tabValue, setTabValue] = useState("Ongoing");
@@ -26,7 +31,7 @@ const KPIScreen = () => {
     setArchivedList([]);
   }, []);
 
-  const ongoingData = [
+  const questionData = [
     {
       id: 1,
       year: 2024,
@@ -43,21 +48,46 @@ const KPIScreen = () => {
         { id: 3, position_name: "Back-End Developer" },
       ],
       division: "Tech",
-    },
-    {
-      id: 2,
-      year: 2024,
-      period: 3,
-      type: "Appraisal",
-      startDate: "2024-07-01",
-      endDate: "2024-09-30",
-      division: [
+      questions: [
         {
-          id: 1,
-          division_name: "Tech",
+          title: "Manpower Fulfillment as per MPP Approval & Request",
+          achievement: [
+            {
+              name: "actual",
+              value: 0,
+            },
+            {
+              name: "actual",
+              value: 0,
+            },
+          ],
         },
-        { id: 2, division_name: "Finance" },
-        { id: 3, division_name: "Sales" },
+        {
+          title: "Feedback Rating For The Townhall Event has been held",
+          achievement: [
+            {
+              name: "actual",
+              value: 0,
+            },
+            {
+              name: "actual",
+              value: 0,
+            },
+          ],
+        },
+        {
+          title: "Employee Workshop (3 times in a year)",
+          achievement: [
+            {
+              name: "actual",
+              value: 0,
+            },
+            {
+              name: "actual",
+              value: 0,
+            },
+          ],
+        },
       ],
     },
   ];
@@ -65,24 +95,52 @@ const KPIScreen = () => {
   return (
     <SafeAreaView style={{ backgroundColor: "#ffffff", flex: 1 }}>
       <View style={styles.header}>
-        <PageHeader width={200} title="KPI" backButton={true} onPress={() => navigation.goBack()} />
+        <PageHeader width={200} title="Employee KPI" backButton={true} onPress={() => navigation.goBack()} />
+      </View>
+      <View
+        style={{
+          paddingVertical: 10,
+          paddingHorizontal: 15,
+          borderWidth: 1,
+          borderColor: "#E2E2E2",
+        }}
+      >
+        <View style={{ gap: 10 }}>
+          <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+            <Text
+              style={[
+                {
+                  paddingVertical: 5,
+                  paddingHorizontal: 15,
+                  backgroundColor: "#D9D9D9",
+                  borderRadius: 15,
+                  textAlign: "center",
+                },
+                TextProps,
+              ]}
+            >
+              Pending
+            </Text>
+            <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+              <Text style={[{ opacity: 0.5 }, TextProps]}>{dayjs("2024-07-01").format("DD MMM YYYY")} to</Text>
+              <Text style={[{ opacity: 0.5 }, TextProps]}>{dayjs("2024-09-30").format("DD MMM YYYY")}</Text>
+            </View>
+          </View>
+          <View>
+            <Text style={[{ opacity: 0.5 }, TextProps]}>Position</Text>
+            <Text style={[TextProps]}>Front End Developer</Text>
+          </View>
+        </View>
       </View>
 
       <View style={styles.container}>
         <View style={{ flex: 1, paddingHorizontal: 15 }}>
           <FlashList
-            data={ongoingData}
+            data={questionData}
             estimatedItemSize={50}
             onEndReachedThreshold={0.1}
             keyExtractor={(item, index) => index}
-            renderItem={({ item, index }) => (
-              <KPIDetailItem
-                key={index}
-                start_date={item?.startDate}
-                end_date={item?.endDate}
-                navigation={navigation}
-              />
-            )}
+            renderItem={({ item, index }) => <KPIDetailItem question={item?.questions} navigation={navigation} />}
           />
         </View>
       </View>
