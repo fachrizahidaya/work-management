aabc3fb80ddb6de0f2d9696f0b8b1c4c63230fa6
jrmygp <fat.snorlax17@gmail.com>
2024-02-16 11:58:50 +0700

    UI : swipable tab view in task list and project list

--- src/screens/Band/ProjectList.js
+++ src/screens/Band/ProjectList.js
@@ -1,10 +1,11 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 
-import { SafeAreaView, StyleSheet, View, Pressable } from "react-native";
+import { SafeAreaView, StyleSheet, View, Pressable, TouchableOpacity, useWindowDimensions, Text } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { FlashList } from "@shopify/flash-list";
 import { RefreshControl } from "react-native-gesture-handler";
+import { TabView, SceneMap } from "react-native-tab-view";
 
 import ProjectListItem from "../../components/Band/Project/ProjectList/ProjectListItem";
 import { useFetch } from "../../hooks/useFetch";
@@ -12,9 +13,8 @@ import PageHeader from "../../components/shared/PageHeader";
 import EmptyPlaceholder from "../../components/shared/EmptyPlaceholder";
 import ProjectSkeleton from "../../components/Band/Project/ProjectList/ProjectSkeleton";
 import useCheckAccess from "../../hooks/useCheckAccess";
-import ProjectFilter from "../../components/Band/Project/ProjectFilter/ProjectFilter";
-import Tabs from "../../components/shared/Tabs";
 import Pagination from "../../components/shared/Pagination";
+import ProjectFilter from "../../components/Band/Project/ProjectFilter/ProjectFilter";
 
 const ProjectList = () => {
   const navigation = useNavigation();
@@ -25,7 +25,6 @@ const ProjectList = () => {
   const [searchInput, setSearchInput] = useState("");
   const [selectedPriority, setSelectedPriority] = useState("");
   const [deadlineSort, setDeadlineSort] = useState("asc");
-  const [tabValue, setTabValue] = useState("On Progress");
   const createActionCheck = useCheckAccess("create", "Projects");
 
   const dependencies = [status, currentPage, searchInput, selectedPriority, deadlineSort, ownerName];
@@ -42,18 +41,6 @@ const ProjectList = () => {
   };
   const { data, isLoading, isFetching, refetch } = useFetch("/pm/projects", dependencies, params);
 
-  const tabs = [
-    { title: "Open", value: "Open" },
-    { title: "On Progress", value: "On Progress" },
-    { title: "Finish", value: "Finish" },
-    { title: "Archived", value: "Archived" },
-  ];
-
-  const onChangeTab = useCallback((value) => {
-    setTabValue(value);
-    setStatus(value);
-  }, []);
-
   const renderSkeletons = () => {
     const skeletons = [];
     for (let i = 0; i < 2; i++) {
@@ -61,6 +48,85 @@ const ProjectList = () => {
     }
     return skeletons;
   };
+
+  const renderFlashList = () => {
+    return !isLoading ? (
+      data?.data?.data?.length > 0 ? (
+        <>
+          <View style={{ flex: 1 }}>
+            <FlashList
+              refreshControl={<RefreshControl refreshing={isFetching} onRefresh={refetch} />}
+              data={data?.data.data}
+              keyExtractor={(item) => item.id}
+              onEndReachedThreshold={0.1}
+              estimatedItemSize={77}
+              renderItem={({ item }) => (
+                <ProjectListItem
+                  id={item.id}
+                  title={item.title}
+                  status={item.status}
+                  deadline={item.deadline}
+                  isArchive={item.archive}
+                  image={item.owner_image}
+                  ownerName={item.owner_name}
+                  ownerEmail={item.owner_email}
+                />
+              )}
+            />
+          </View>
+
+          {data?.data?.last_page > 1 && (
+            <Pagination data={data} setCurrentPage={setCurrentPage} currentPage={currentPage} />
+          )}
+        </>
+      ) : (
+        <EmptyPlaceholder height={200} width={240} text="No project" />
+      )
+    ) : (
+      <View style={{ display: "flex", paddingHorizontal: 2, gap: 2 }}>{renderSkeletons()}</View>
+    );
+  };
+
+  const renderScene = SceneMap({
+    open: renderFlashList,
+    onProgress: renderFlashList,
+    finish: renderFlashList,
+    archive: renderFlashList,
+  });
+
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = useState(1);
+  const [routes] = useState([
+    { key: "open", title: "Open" },
+    { key: "onProgress", title: "On Progress" },
+    { key: "finish", title: "Finish" },
+    { key: "archive", title: "Archived" },
+  ]);
+
+  const renderTabBar = (props) => (
+    <View style={{ display: "flex", flexDirection: "row", backgroundColor: "white" }}>
+      {props.navigationState.routes.map((route, i) => (
+        <TouchableOpacity
+          key={i}
+          style={{
+            flex: 1,
+            height: 48,
+            justifyContent: "center",
+            alignItems: "center",
+            borderBottomWidth: 2,
+            borderBottomColor: index === i ? "#176688" : "#E8E9EB",
+          }}
+          onPress={() => {
+            setIndex(i);
+            setStatus(route.title);
+          }}
+        >
+          <Text style={{ color: index === i ? "#176688" : "black" }}>{route.title}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
 
   useEffect(() => {
     setCurrentPage(1);
@@ -111,45 +177,31 @@ const ProjectList = () => {
             selectedPriority={selectedPriority}
           />
 
-          <View style={{ paddingHorizontal: 16 }}>
-            <Tabs tabs={tabs} value={tabValue} onChange={onChangeTab} />
-          </View>
-
-          {!isLoading ? (
-            data?.data?.data?.length > 0 ? (
-              <>
-                <View style={{ flex: 1 }}>
-                  <FlashList
-                    refreshControl={<RefreshControl refreshing={isFetching} onRefresh={refetch} />}
-                    data={data?.data.data}
-                    keyExtractor={(item) => item.id}
-                    onEndReachedThreshold={0.1}
-                    estimatedItemSize={200}
-                    renderItem={({ item }) => (
-                      <ProjectListItem
-                        id={item.id}
-                        title={item.title}
-                        status={item.status}
-                        deadline={item.deadline}
-                        isArchive={item.archive}
-                        image={item.owner_image}
-                        ownerName={item.owner_name}
-                        ownerEmail={item.owner_email}
-                      />
-                    )}
-                  />
-                </View>
-
-                {data?.data?.last_page > 1 && (
-                  <Pagination data={data} setCurrentPage={setCurrentPage} currentPage={currentPage} />
-                )}
-              </>
-            ) : (
-              <EmptyPlaceholder height={200} width={240} text="No project" />
-            )
-          ) : (
-            <View style={{ display: "flex", paddingHorizontal: 2, gap: 2 }}>{renderSkeletons()}</View>
-          )}
+          <TabView
+            navigationState={{ index, routes }}
+            renderScene={renderScene}
+            onIndexChange={(index) => {
+              setIndex(index);
+              switch (index) {
+                case 0:
+                  setStatus("Open");
+                  break;
+                case 1:
+                  setStatus("On Progress");
+                  break;
+                case 2:
+                  setStatus("Finish");
+                  break;
+                case 3:
+                  setStatus("Archived");
+                  break;
+                default:
+                  setStatus("Open");
+              }
+            }}
+            initialLayout={{ height: 0, width: layout.width }}
+            renderTabBar={renderTabBar}
+          />
         </View>
 
         {createActionCheck && (
