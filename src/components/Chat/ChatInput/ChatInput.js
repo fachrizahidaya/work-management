e9fc569fc460e0d159eea0d36ad77a402b894bde
Fachriza Hidaya <fachriza.theblues@gmail.com>
2024-03-21 08:43:15 +0700

    chore: remove unnecessary

--- src/components/Chat/ChatInput/ChatInput.js
+++ src/components/Chat/ChatInput/ChatInput.js
@@ -170,7 +170,6 @@ const ChatInput = ({
         setStatus("processing");
         onSendMessage(formData, setSubmitting, setStatus);
       }
-      resetForwardedBandAttachment();
       resetForm();
       setFileAttachment(null);
       setForwardedBandAttachment(null);
